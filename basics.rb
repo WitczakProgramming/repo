18c33af9d2f557a84c474915db0322954ae92654
WitczakProgramming <critical.error@hotmail.com>
2016-03-05 20:54:25 -0800

    test commit

---
+++ basics.rb
@@ -0,0 +1,13 @@
+# examples of symbols
+:name
+:a_symbol
+:"this is also a symbol because it has the colon"
+# examples of integers
+1, 2, 3, 50, 10, 439034890
+# examples of floats
+1.2345, 2354.4567, 349.5964
+# example of string interpolation
+a = 'ten'
+"my favorite number is #{a}!"
+# 
+1 + 2
